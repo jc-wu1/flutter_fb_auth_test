0ebce231a17b35f3b878b9918c81f0a6bdf8681d
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2022-02-15 14:39:16 +0700

    added fb auth

--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -10,26 +11,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Map<String, dynamic>? _userData;
+  AccessToken? _accessToken;
+  bool _checking = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfIsLogged();
+  }
+
+  Future<void> _checkIfIsLogged() async {
+    final accessToken = await FacebookAuth.instance.accessToken;
+    setState(() {
+      _checking = false;
+    });
+    if (accessToken != null) {
+      print("is Logged:::: ${accessToken.toJson()}");
+      final userData = await FacebookAuth.instance.getUserData();
+
+      _accessToken = accessToken;
+      setState(() {
+        _userData = userData;
+      });
+    }
+  }
+
+  Future<void> _login() async {
+    final LoginResult result = await FacebookAuth.instance.login();
+
+    if (result.status == LoginStatus.success) {
+      _accessToken = result.accessToken;
+      print(_accessToken);
+
+      final userData = await FacebookAuth.instance.getUserData();
+
+      _userData = userData;
+    } else {
+      print(result.status);
+      print(result.message);
+    }
+
+    setState(() {
+      _checking = false;
+    });
+  }
+
+  Future<void> _logOut() async {
+    await FacebookAuth.instance.logOut();
+    _accessToken = null;
+    _userData = null;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Facebook Auth Example'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Click here to FB auth',
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('FB Login'),
+      body: _checking
+          ? const Center(
+              child: CircularProgressIndicator(),
             )
-          ],
-        ),
-      ),
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      _userData != null ? _userData.toString() : "NO LOGGED",
+                    ),
+                    const SizedBox(height: 20),
+                    _accessToken != null
+                        ? Text(
+                            _accessToken!.toJson().toString(),
+                          )
+                        : Container(),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      child: Text(
+                        _userData != null ? "LOGOUT" : "LOGIN",
+                      ),
+                      onPressed: _userData != null ? _logOut : _login,
+                    ),
+                    const SizedBox(height: 50),
+                  ],
+                ),
+              ),
+            ),
     );
   }
 }
