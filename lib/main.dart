213f2b729a8e745aae0aa5327ad3cf4fff234414
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2022-02-15 09:32:26 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fb_auth_test/src/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ lib/src/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fb_auth_test/src/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
