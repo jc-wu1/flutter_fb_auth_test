8a045ddb87abaee6df2f7b276d44de0df92fb913
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2022-02-15 15:25:37 +0700

    update google auth

--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -14,11 +24,20 @@ class _MyHomePageState extends State<MyHomePage> {
   Map<String, dynamic>? _userData;
   AccessToken? _accessToken;
   bool _checking = true;
+  GoogleSignInAccount? _currentUser;
+  String _contactText = '';
 
   @override
   void initState() {
     super.initState();
     _checkIfIsLogged();
+
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+    });
+    _googleSignIn.signInSilently();
   }
 
   Future<void> _checkIfIsLogged() async {
@@ -37,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Future<void> _login() async {
+  Future<void> _loginFB() async {
     final LoginResult result = await FacebookAuth.instance.login();
 
     if (result.status == LoginStatus.success) {
@@ -57,10 +76,24 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  Future<void> _logOut() async {
+  Future<void> _logOutFB() async {
     await FacebookAuth.instance.logOut();
     _accessToken = null;
     _userData = null;
+    setState(() {});
+  }
+
+  Future<void> _loginGoogle() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  Future<void> _logoutGoogle() async {
+    _googleSignIn.disconnect();
+    _currentUser = null;
     setState(() {});
   }
 
@@ -68,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Facebook Auth Example'),
+        title: const Text('Auth Example'),
       ),
       body: _checking
           ? const Center(
@@ -79,24 +112,59 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text(
-                      _userData != null ? _userData.toString() : "NO LOGGED",
-                    ),
-                    const SizedBox(height: 20),
-                    _accessToken != null
-                        ? Text(
-                            _accessToken!.toJson().toString(),
+                    // Text(
+                    //   _userData != null
+                    //       ? _userData.toString()
+                    //       : "NOT LOGGED IN",
+                    // ),
+                    _userData != null
+                        ? ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                _userData!["picture"]["data"]["url"],
+                              ),
+                            ),
+                            title: Text(_userData!["name"]),
+                            subtitle: Text(_userData!["email"]),
                           )
-                        : Container(),
+                        : Text(
+                            'Not logged in',
+                          ),
                     const SizedBox(height: 20),
                     TextButton(
                       child: Text(
-                        _userData != null ? "LOGOUT" : "LOGIN",
+                        _userData != null
+                            ? "Logout Facebook"
+                            : "Login via Facebook",
                       ),
-                      onPressed: _userData != null ? _logOut : _login,
+                      onPressed: _userData != null ? _logOutFB : _loginFB,
                     ),
                     const SizedBox(height: 50),
+                    _currentUser != null
+                        ? ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                _currentUser!.photoUrl!,
+                              ),
+                            ),
+                            title: Text(_currentUser!.displayName!),
+                            subtitle: Text(_currentUser!.email),
+                          )
+                        : Text(
+                            'Not logged in',
+                          ),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      child: Text(
+                        _currentUser != null
+                            ? "Logout Google"
+                            : "Login via Google",
+                      ),
+                      onPressed:
+                          _currentUser != null ? _logoutGoogle : _loginGoogle,
+                    ),
                   ],
                 ),
               ),
